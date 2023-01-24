6a4d6cdaf8dcd6b7efc12d97bc7412162b066221
Garima223 <110466128+Garima223@users.noreply.github.com>
2023-01-24 15:07:57 +0530

    Update Consultant.js

--- Consultant.js
+++ Consultant.js
@@ -12,14 +12,16 @@ route.get("/list",(req,res)=>{
 route.post("/create",(req,res)=>{
 
     const json={
-        product_name:'Aakash',
-        product_extinguisher_type:'pet2',
-        product_extinguisher_capacity:'pec1',
-        product_manufactured_date:new Date(),
-        product_due_date: new Date(),
-        product_remarks:'pr1',
-        product_client_id:'pci1',
-        product_consultant_id:'pci1'
+        consultant_name:'Komal & Garima',
+        consultant_email: 'abc132@gmail.com',
+        consultant_password: '123abc',
+        consultant_city: 'Indore',
+        consultant_state: 'Madhya Pradesh',
+        consultant_address: 'Indore',
+        consultant_phone: '0987654321',
+        consultant_other_info: 'optional',
+        consultant_date: 24-01-2023;
+        
     }
 
     new db(json).save()
@@ -40,15 +42,17 @@ route.put("/edit",(req,res)=>{
     const id='63ce09aa3f7ef04cd819f78d'
 
     const json={
-        product_name:'Aakash',
-        product_extinguisher_type:'pet2',
-        product_extinguisher_capacity:'pec1',
-        product_manufactured_date:new Date(),
-        product_due_date: new Date(),
-        product_remarks:'pr1',
-        product_client_id:'pci1',
-        product_consultant_id:'pci1'
-    }
+         consultant_name:'Komal & Garima',
+        consultant_email: 'abc132@gmail.com',
+        consultant_password: '123abc',
+        consultant_city: 'Indore',
+        consultant_state: 'Madhya Pradesh',
+        consultant_address: 'Indore',
+        consultant_phone: '0987654321',
+        consultant_other_info: 'optional',
+        consultant_date: 24-01-2023;
+       
+            }
 
     db.findOneAndUpdate({_id:id},json,{upsert: true}, function(err, doc){
 
@@ -70,4 +74,4 @@ route.get("/export",(req,res)=>{
     res.send('export')
 })
 
-module.exports = route
+module.exports = consultant;
