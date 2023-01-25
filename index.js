9c160b75890fdafc9c24a97c7acedccc4e13e0d5
Garima223 <110466128+Garima223@users.noreply.github.com>
2023-01-25 14:25:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const connectToMongo = require('./mongodb.js');
+//const routes = require('./routes/Consultant.js');
 const express = require('express');
 var cors = require('cors');
-// connectToMongo()
+const connectDB = require('./mongodb.js');
+//connectToMongo();
 const app = express()
 const port = 5000
 app.use(express.json())
@@ -11,11 +12,26 @@ app.use(cors())
 //app.use('/api/product', require('./routes/product'))
 // app.use('/api/inspector, require('./routes/inspector))
 app.use('/api/Consultant', require('./routes/Consultant'))
+
+
+const start = async () =>{
+    try{
+       await connectDB(); 
+       console.log("Connected");
+    }catch (error){
+        console.log(error);
+    }
+}
+
+
 // app.use('/api/inspection, require('./routes/inspection))
 // app.use('/api/admin, require('./routes/admin))
 app.get('/', (req, res) => {
 res.send('Hello World!')
 })
-app.listen(port, () => {
+/*app.listen(port, () => {
 console.log(`iNotebook backend listening at port http://localhost:${port}`)
-})
+})*/
+app.listen(port, () => {
+    console.log(`${port} yes I am connected`);
+    });
