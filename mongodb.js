3407fbf73c0d1a09af1becb231fdfad80a44a350
Garima223 <110466128+Garima223@users.noreply.github.com>
2023-01-25 14:53:20 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-uri = "mongodb+srv://Komal:QUSkYpKsQcHXPrvf@consultantapi.vzinfil.mongodb.net/ConsultantApi?retryWrites=true&w=majority"
+//uri = "mongodb+srv://Komal:QUSkYpKsQcHXPrvf@consultantapi.vzinfil.mongodb.net/ConsultantApi?retryWrites=true&w=majority"
+const mongoURI = 'mongodb://127.0.0.1:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+mongoose.set('strictQuery', true);
 
 const connectDB = () => {
     return mongoose.connect(uri, {
